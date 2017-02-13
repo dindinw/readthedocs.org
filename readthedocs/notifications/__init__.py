5d10d29ea4e037fe076967ae38fa9276cc5830ee
Alex Wu <wuyiding@gmail.com>
2017-02-14 00:10:05 +0800

    test rdc

--- readthedocs/notifications/__init__.py
+++
@@ -1,4 +0,0 @@
-from .notification import Notification
-from .backends import send_notification
-
-default_app_config = 'readthedocs.notifications.apps.NotificationsAppConfig'
--- readthedocs/notifications/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotificationsAppConfig(AppConfig):
-    name = 'readthedocs.notifications'
-    verbose_name = 'Notifications'
